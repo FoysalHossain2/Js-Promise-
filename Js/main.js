a9e08eace3a5297a526101dcb115dbb8aeae377e
FoysalHossain2 <fh375170@gmail.com>
2024-02-20 07:46:33 +0600

    js Loops

--- Js/main.js
+++ Js/main.js
@@ -1,121 +1,173 @@
-// const promiseOne = new Promise(function(resolve, reject) {
-//     setTimeout(function() {
-//         console.log('Async task is complete');
-//         resolve()       
-//     })
-// }, 1000)
+// // for loop
 
-// promiseOne.then(function(){
-//     console.log("Promise consumed")
-// })
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     if(element === 5){
+//        // console.log("5 is the best");
+//     }
 
-// new Promise (function(resolve, reject) {
-//     setTimeout(function(){
-//         console.log("Async task 2");
-//         resolve()
-//     })
-// }).then(function(){
-//     console.log("Async 2 resolved")
-// })
+//    // console.log(element);
+    
+// }
 
-
-// const promiseThree = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         resolve({userName:"Foysal", email:"chai@example.com"})
-//     },1000)
-// })    
-
-// promiseThree.then(function(user){
-//     console.log(user);
-// })
-
-
-// const promiseFour = new promise(function(resolve, reject){
-//     setTimeout(function(){
-//         let error = true
-//         if(!error) {
-//             resolve({username:"Foysal Hossain", password:"123"})
-//         } else {
-//             reject('Error: Something went wrong')
-//         }
-//     },1000)
-// })
-
-
-// ************promise practice2*********************//
-// const state = true;
-
-// console.log('Task 1');
-
-// const promise = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         if(state) {
-//             resolve('Task 2')
-//         }else {
-//             reject('failed');
-//         }
-//     },2000)
-// });
-
-// promise 
-// 	.then(function(value){
-//         console.log(value);
-//     })
-//     .catch(function(error){
-//         console.log(error);
-//     });
-
-// console.log('Task 3')
-
-
-
-
-// ************Promise || Asynchronous Implement*****************\\
-
-const paymentSuccess = true;
-const marks = 80;
-
-function enroll(){
-    console.log('course enrollment is in progress');
-
-    const promise = new Promise(function(resolve, reject){
-        setTimeout(function(){
-            if(paymentSuccess){
-                resolve();
-            } else {
-                reject('Payment failed');
-            }
-       },2000)
-    })
-
-    return promise;
-}
-
-function progress(){
-    console.log('Course on progress...')
-
-    const promise = new Promise(function(resolve, reject){
+// for(let i = 1; i<= 10; i++){
+//     //  console.log(`outer loop value: ${i}`);
+//      for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+//         // console.log(i + '*' + j + '=' + i+j);
         
-    setTimeout(function(){
-        if(marks >=80){
-            resolve();
-        } else {
-            reject("You could not get enough marks to get certificate");
-        }
-    },3000)
-    })
+//      }
+// }
 
-    return promise;
+
+
+// // Break
+
+// // for (let index = 0; index <= 20; index++) {
+// //     if (index === 5) {
+// //         console.log('Detected 5');
+// //         break
+// //     }
+
+// //     console.log('value of is ', index);
+    
+// // }
+
+// for (let index = 0; index <= 20; index++) {
+//     if (index === 5) {
+//         // console.log('Detected 5');
+//         continue 
+//     }
+
+//     // console.log('value of is ', index);
+    
+// }
+
+
+
+
+// /**** While loop *****/
+
+// let index = 0
+// while (index <= 10) {
+//     // console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ['flash', 'batman' , 'supperMan']
+
+// let arr = 0
+// while (arr < myArray.length){
+//     // console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+
+
+
+
+// /* for of loop */
+// // ["","",""]
+// // [{},{},{}]
+
+// const myArr = [1,2,3,4,5]
+
+// for (const num of myArr) {
+//         // console.log(num);
+// }
+
+// const greetings = 'Hello world'
+// for (const greet of greetings) {
+//     // console.log(`Each char of ${greet}`);
+// }
+
+
+
+// // object
+// const myObject = {
+//     js: 'javaScript',
+//     cpp: 'C++',
+//     rb: 'ruby',
+//     py: 'python'
+// }
+
+// for (const key in myObject) {
+//     console.log(myObject[key]);
+// }
+
+
+
+
+
+
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {    
+    // console.log(num);
 }
 
-function getCertificate(){
-    console.log('Preparing your certificate');
 
-    const promise = new Promise(function(resolve){
-        setTimeout(function(){
-            resolve('Congrats! You got the certificate');
-        },1000)
-    })
+const greetings = "Hello world"
 
-    return promise;
+for (const greet of greetings) {
+    // console.log(`Each char is ${greet}`);
 }
+
+
+
+const map = new Map()
+map.set('In', 'India')
+map.set('USA', 'United States of America')
+map.set('BN' , 'Bangladesh')
+
+for (const [key, value] of map) {
+    // console.log(key, ':-' , value);
+}
+
+
+const myObject = {
+    'js':"javascript",
+    'cpp': "C++",
+    'py': "Python",
+    "ruby": "ruby"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is ${myObject[key]}`);
+}
+
+const coding = ['js','ruby', 'java', 'çpp', 'python']
+
+// coding.forEach( function(val){
+//     console.log(val);
+// })
+
+// coding.forEach((val) =>{
+//     console.log(val);
+// })
+
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "Java"
+    },
+    {
+        languageName: "Cpp",
+        languageFileName: "C++"
+    }
+]
+
+
+myCoding.forEach((items)=>{
+    console.log(items.languageName);
+})
